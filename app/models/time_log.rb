5bb439319e4a92ccc9cb90dd1efb82b386a716f2
ngocvt <ngocvu.uet@gmail.com>
2016-06-20 12:50:14 +0700

    fix bug when change date

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,7 +1,7 @@
 class TimeLog < ActiveRecord::Base
   belongs_to :sprint
   belongs_to :user
-  belongs_to :assignee
+  belongs_to :assignee, dependent: :destroy
   belongs_to :master_sprint
 
   scope :total_lost_hour, ->master_sprint do
